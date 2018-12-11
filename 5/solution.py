7582571b9d5128b951e6ccc8549a55083b1d8032
Gary Baalman <nospamplease@nym.hush.com>
2018-12-11 08:11:43 -0800

    clean up challenge 5

--- 5/solution.py
+++ 5/solution.py
@@ -4,40 +4,22 @@
 
 import webbrowser, requests, re
 
-url = "http://www.pythonchallenge.com/pc/def/equality.html"
+firsturl = "http://www.pythonchallenge.com/pc/def/linkedlist.php?nothing=63579"
 baseurl = "http://www.pythonchallenge.com/pc/def/"
 
-def gettext():
+def gettext(url):
   # pull down the HTML from the link
   r = requests.get(url)
   return r.text
 
-def findcomments(body):
-  # returns an array of text strings that 
-  # were surrounded by <!-- and -->
-  # assumes well-formed comments, won't deal with nested ones
-  comments = []
-  comment = ""
-  arewethereyet = False
-  for line in body.splitlines():
-    if line == "<!--":
-      arewethereyet = True
-      continue
-    if line == "-->":
-      arewethereyet = False
-      comments.append(comment)
-      comment = ''
-    if arewethereyet == True:
-      comment += line
-  return(comments)
-
 def main():
-  answer = ''
-  source = findcomments(gettext())[0]
-  matches=re.findall(r'[a-z][A-Z]{3}[a-z][A-Z]{3}[a-z]', source)
-  for item in matches:
-    answer += item[4]
-  webbrowser.open(baseurl+answer+".html")
+  i = 0
+  url=firsturl
+  while i < 400:
+    body = gettext(url)
+    print(body)
+    url=baseurl+"linkedlist.php?nothing="+body.split()[-1]
+    i += 1
       
 if __name__== "__main__":
   main()
